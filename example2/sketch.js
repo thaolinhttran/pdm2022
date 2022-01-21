aea83ec5a2dae2d59c0c7a20c4576dc7345b7dfe
Linh Tran <thaolinhttran@gmail.com>
2022-01-21 10:53:07 -0600

    assignment1_example2

---
+++ example2/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  noStroke();
+  fill(255, 0, 0, 100);
+  circle(200, 150, 150);
+  fill(0, 0, 255, 100);
+  circle(150, 250, 150);
+  fill(0, 255, 0, 100);
+  circle(250, 250, 150);
+}
