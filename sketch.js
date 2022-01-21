ef9e8202ef8c1c5ff4e13271872c8f57a54e5cc4
Linh Tran <thaolinhttran@gmail.com>
2022-01-21 10:06:57 -0600

    assignment1_example1

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 300);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("#0f0");
+  strokeWeight(1);
+  fill(255);
+  circle(150, 150, 250);
+  rect(450, 150, 250, 250);
+}
